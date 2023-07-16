c9a16fa3938e9b5f98b97afe8319f297e6120c7c
Kiran Bharadwaj H D <hdkiranbharadwaj@gmail.com>
2023-07-16 08:20:13 +0530

    new file:   pr3/output/load 	new file:   pr3/output/selection.exe 	modified:   pr3/selection.c

--- pr3/selection.c
+++ pr3/selection.c
@@ -26,23 +26,29 @@ void analyser()
         {
             *(search_array + i) = rand() % size;
         }
-        o = selection_sort(search_array, size);
+        = selection_sort(search_array, size);
         printf("%d\n", o);
         fprintf(fp2, "%d\t%d\n", size, o);
         free(search_array);
     }
+    /*int sa[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    selection_sort(sa, 10);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\t", sa[i]);
+    }*/
     fclose(fp2);
 }
 int selection_sort(int *a, int size)
 {
     int count = 0, i, j, temp, min;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size - 1; i++)
     {
         min = i;
-        for (j = 0; j < size - i - 1; j++)
+        for (j = i + 1; j < size; j++)
         {
             count++;
-            if (*(a + j) > *(a + min))
+            if (*(a + j) < *(a + min))
             {
                 min = j;
             }
